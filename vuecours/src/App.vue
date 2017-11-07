faaee851c8e22dc0e7a661273c2163d60beb1749
Anne Remacle <anne.m.remacle@gmail.com>
2017-11-07 11:52:44 +0100

    externalize a component for the title of the page

--- vuecours/src/App.vue
+++ vuecours/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    
+        <page-title></page-title>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import PageTitle from './PageTitle.vue';
+
+    export default {
+      name: 'app',
+      components: {
+          PageTitle
+      }
     }
-  }
-}
 </script>
+
+<!-- on peut se permettre d'écrire juste le nom du component dans les components parce qu'il a le même nom que le fichier -->
---
+++ vuecours/src/PageTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+    export default {
+      name: "page-title",
+      data(){
+        return {
+            title: "Hello world!"
+        }
+      }
+    }
+</script>
